73f515606114b0c0c9e0575c2156d569e9ea1b16
Khanh Tran <42185297+khanhtran2000@users.noreply.github.com>
2020-08-09 13:31:15 +0700

    Add files via upload

---
+++ Text Normalization/Web Crawlers/front_page_crawler.py
@@ -0,0 +1,39 @@
+from vnexpress_crawler import Extract_Text, Extract_URLs, concat_dataframe
+
+# Output path
+out_path = "/Users/macbook/Desktop/Everything Deep Learning/FPT.AI 2020/Text Normalization/raw_text_vnexpress.csv"
+
+# Parse the front page
+url =  'https://vnexpress.net'
+chrome_path = "/Users/macbook/Desktop/Everything Deep Learning/FPT.AI 2020/Text Normalization/chromedriver"
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+
+extract_urls = Extract_URLs()
+extract_text = Extract_Text()
+
+# Top story
+top_url = extract_urls.get_top_story_url(soup)
+top_text = extract_text.get_text_from_url(top_url)
+# Sub top stories
+sub_urls = extract_urls.get_sub_stories_urls(soup)
+sub_text = extract_text.get_text_from_url(sub_urls)
+# View article
+view_url = extract_urls.get_view_url(soup)
+view_text = extract_text.get_text_from_view(view_url)
+# Left column stories
+left_urls = extract_urls.get_col_left_urls(soup)
+left_text = extract_text.get_text_from_url(left_urls)
+# Right column stories
+right_urls = extract_urls.get_col_right_urls(url, chrome_path)
+right_text = extract_text.get_text_from_url(right_urls)
+# Below articles 
+below_urls = extract_urls.get_below_articles_urls(url, chrome_path)
+below_text = extract_text.get_text_from_url(below_urls)
+
+# Combine crawl dictionaries
+crawls = [top_text, sub_text, view_text, left_text, right_text, below_text]
+
+# # Export crawl
+final_crawl = concat_dataframe(crawls)
+final_crawl.to_csv(out_path)
